987087086e2099a44131fb718885fa9859216e42
icy-blue <icy@luogu.org>
2019-12-22 11:21:10 +0800

    Create KMP.java

---
+++ src/sql/functions/KMP.java
@@ -0,0 +1,44 @@
+package sql.functions;
+
+import org.jetbrains.annotations.NotNull;
+
+public class KMP {
+
+    //str文本串  dest 模式串
+    public static int kmp(@NotNull String str, String dest, int[] next) {
+        for (int i = 0, j = 0; i < str.length(); i++) {
+            while (j > 0 && str.charAt(i) != dest.charAt(j)) {
+                j = next[j - 1];
+            }
+            if (str.charAt(i) == dest.charAt(j)) {
+                j++;
+            }
+            if (j == dest.length()) {
+                return i - j + 1;
+            }
+        }
+        return 0;
+    }
+
+    @NotNull
+    public static int[] kmpNext(@NotNull String dest) {
+        int[] next = new int[dest.length()];
+        next[0] = 0;
+        for (int i = 1, j = 0; i < dest.length(); i++) {
+            while (j > 0 && dest.charAt(j) != dest.charAt(i)) {
+                j = next[j - 1];
+            }
+            if (dest.charAt(i) == dest.charAt(j)) {
+                j++;
+            }
+            next[i] = j;
+        }
+        return next;
+    }
+
+    public static boolean findString(@NotNull String str, @NotNull String toFind) {
+        int[] next = kmpNext(str);
+        return kmp(toFind, str, next) > 0;
+    }
+
+}
